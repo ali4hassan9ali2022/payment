be0a17c543b0f72a10458c82e204cd15f4bedd48
alikasrawy <kasrawyali47@gmail.com>
2025-04-25 19:05:33 +0300

    create assets file to my app

--- lib/Featured/checkout/Presentation/Views/my_card_view.dart
+++ lib/Featured/checkout/Presentation/Views/my_card_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payment_app/Core/Utils/app_style.dart';
 
 class MyCardView extends StatelessWidget {
   const MyCardView({super.key});
@@ -7,17 +8,10 @@ class MyCardView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "My Card",
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 25,
-            fontFamily: "Inter",
-            fontWeight: FontWeight.w500,
-            height: 0,
-          ),
-        ),
-      ),
+        leading: Icon(Icons.arrow_back_ios_new_outlined),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Text("My Card", style: AppStyle.style25)),
     );
   }
 }
