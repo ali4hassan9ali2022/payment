4d023b9dbb257eb76ffad9dd17d65c982989f057
alikasrawy <kasrawyali47@gmail.com>
2025-04-25 19:23:52 +0300

    create order info item to my card view body

--- lib/Featured/checkout/Presentation/Views/Widgets/my_card_view_body.dart
+++ lib/Featured/checkout/Presentation/Views/Widgets/my_card_view_body.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:payment_app/Core/Utils/assets.dart';
+import 'package:payment_app/Featured/checkout/Presentation/Views/Widgets/order_info_item.dart';
 
 class MyCardViewBody extends StatelessWidget {
   const MyCardViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [SizedBox(height: 18), Image.asset(Assets.imagesBasketImage)],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          SizedBox(height: 18),
+          Image.asset(Assets.imagesBasketImage),
+          SizedBox(height: 25),
+          OrderInfoItem(title: "Order Subtotal", value: r"$42.97"),
+          SizedBox(height: 3),
+          OrderInfoItem(title: "Discount", value: r"$0"),
+          SizedBox(height: 3),
+          OrderInfoItem(title: "Shipping", value: r"$8"),
+        ],
+      ),
     );
   }
 }
